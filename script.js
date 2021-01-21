2254ce8dec04caa0eb86c563fd9449db69605e9e
CPMSP <cpetersonmsp@gmail.com>
2021-01-21 14:19:39 -0600

    added styles and tied out functions

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ let computerSelection;
 
 const userButtons = document.querySelectorAll('.userPlay');
 
-const text = document.querySelector('.textOutput');
-const score = document.querySelector('.scoreOutput');
-const winner = document.querySelector('.winner');
-const actions = document.querySelector('.actions');
-const round = document.querySelector('.round');
+const textContainer = document.querySelector('.textContainer');
+
+const round = document.createElement('p');
+const actions = document.createElement('p');
+const text = document.createElement('p');
+const score = document.createElement('p');
+const winner = document.createElement('p');
 
 const computerPlay = () => {
 	let shoot = Math.random();
@@ -37,45 +39,55 @@ userButtons.forEach((button) => {
 });
 
 const oneRound = (userSelection, computerSelection) => {
-	actions.textContent = `Player Selected: ${userSelection}; Computer Selected: ${computerSelection}`;
+	actions.textContent = `Player: ${userSelection}; Computer: ${computerSelection}`;
+	textContainer.appendChild(actions);
 	if (userSelection === 'rock' && computerSelection === 'scissors') {
 		// console.log('You Win!  Rock smashes Scissors!');
 		text.textContent = 'You Win!  Rock smashes Scissors!';
+		textContainer.appendChild(text);
 		playerScore++;
 	}
 	if (userSelection === 'rock' && computerSelection === 'paper') {
 		// console.log('You Lose!  Paper covers Rock!');
 		text.textContent = 'You Lose!  Paper covers Rock!';
+		textContainer.appendChild(text);
 		compScore++;
 	}
 	if (userSelection === 'paper' && computerSelection === 'rock') {
 		// console.log('You Win!  Paper covers Rock!');
 		text.textContent = 'You Win!  Paper covers Rock!';
+		textContainer.appendChild(text);
 		playerScore++;
 	}
 	if (userSelection === 'paper' && computerSelection === 'scissors') {
 		// console.log('You Lose!  Scissors cuts Paper!');
 		text.textContent = 'You Lose!  Scissors cuts Paper!';
+		textContainer.appendChild(text);
 		compScore++;
 	}
 	if (userSelection === 'scissors' && computerSelection === 'rock') {
 		// console.log('You Lose!  Rock smashes Scissors!');
 		text.textContent = 'You Lose!  Rock smashes Scissors!';
+		textContainer.appendChild(text);
 		compScore++;
 	}
 	if (userSelection === 'scissors' && computerSelection === 'paper') {
 		// console.log('You Win!  Scissors cuts Paper!');
 		text.textContent = 'You Win!  Scissors cuts Paper!';
+		textContainer.appendChild(text);
 		playerScore++;
 	}
 	else if (userSelection === computerSelection) {
 		// console.log('Tie Round!  Shoot Again!');
 		text.textContent = 'Tie Round!  Shoot Again!';
+		textContainer.appendChild(text);
 	}
 	// console.log(`Player: ${playerScore}; Computer: ${compScore}`);
 	score.textContent = `Player: ${playerScore}; Computer: ${compScore}`;
+	textContainer.appendChild(score);
 	roundNum++;
 	round.textContent = `Round # ${roundNum}`;
+	textContainer.appendChild(round);
 	if (playerScore === 5 || compScore === 5) {
 		endGame(playerScore, compScore);
 	}
@@ -97,12 +109,18 @@ const endGame = (playerScore, compScore) => {
 	// 	victor = 'Tie Game';
 	// }
 	// console.log(`Winner is: ${victor} !`);
-	winner.textContent = `Winner is: ${victor}!  Press Reset to Play Again`;
+	winner.textContent = `Winner is: ${victor}!`;
+	textContainer.appendChild(winner);
 	playerScore = 0;
 	compScore = 0;
 	roundNum = 0;
 	victor = '';
+	// score.textContent = '';
 };
+
+// const isWinner = (playerScore, compScore) => {
+// 		(playerScore === 5 || compScore === 5) ?
+// 	}
 
 const reset = document.querySelector('#reset');
 reset.addEventListener('click', () => {
